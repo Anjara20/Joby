d7e69b1456323881c5eaa174f288f990f4f7df48
Anjara20 <andrianilanaliantsoa6533@gmail.com>
2022-07-05 12:13:57 +0300

    001: backend-auth

---
+++ backend/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use GuzzleHttp\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['middleware' => ['cors', 'json.response']], function (){
+
+});
