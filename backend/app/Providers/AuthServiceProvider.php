00e3208544a09ed93ca39fbe684b117a7e1bc4b6
Anjaramamy Liantsoa <andrianilana.liantsoa@gmail.com>
2022-07-06 20:50:29 +0300

    003 set lifetime access token

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
+use Laravel\Passport\Bridge\PersonalAccessGrant;
+use League\OAuth2\Server\AuthorizationServer;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,6 +29,8 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+        $lifetime = new \DateInterval('PT24H');
+        $this->app->get(Authorization::class)->enableGrantType(new PersonalAccessGrant(), $lifetime);
         
     }
 }
