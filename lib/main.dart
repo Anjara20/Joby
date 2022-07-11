9128c8f906f9897df6a9c944598bd5f49590756c
Anthony <tonyrak7@gmail.com>
2022-07-11 21:13:27 +0300

    003 mobile: bearer token modifiee

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project/pageAuth/connexion.dart';
 import 'package:flutter_project/pageAuth/inscription.dart';
+import 'package:flutter_project/pageCrud/accueil.dart';
 import 'package:flutter_project/screens/home_screen.dart';
 import 'package:lottie/lottie.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp());
 
@@ -34,5 +37,42 @@ class Home extends StatelessWidget {
         backgroundColor: Color.fromARGB(255, 17, 142, 201),
         animationDuration: Duration(seconds: 1),
         nextScreen: Inscription());
+  }
+}
+
+class CheckAuth extends StatefulWidget {
+  @override
+  _CheckAuthState createState() => _CheckAuthState();
+}
+
+class _CheckAuthState extends State<CheckAuth> {
+  bool isAuth = false;
+  @override
+  void initState() {
+    _checkIfLoggedIn();
+    super.initState();
+  }
+
+  void _checkIfLoggedIn() async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    var token = localStorage.getString('token');
+    if (token != null) {
+      setState(() {
+        isAuth = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget child;
+    if (isAuth) {
+      child = Accueil();
+    } else {
+      child = Connexion();
+    }
+    return Scaffold(
+      body: child,
+    );
   }
 }
